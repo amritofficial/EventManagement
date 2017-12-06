207741f55f2514ab0ff90fb61fbba8c324b7c1a9
Amrit Singh <amrit.singh@rbc.com>
2017-12-06 11:17:16 -0500

    Search bar working

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.css']
 })
-export class ContentComponent implements OnInit {
+export class ContentComponent implements OnInit, OnChanges {
+  @Input() searchThis: string;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngOnChanges() {
+    console.log("Content Component: "+this.searchThis);
   }
 
 }
